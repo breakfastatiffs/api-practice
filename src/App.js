406df69cc1244f9ae433de3ca3c1c97dcbe7029c
Tiff Joy <tiffanysummerford@gmail.com>
2021-02-23 14:31:20 -0500

    rendering data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import Item from './Item'
 
 export default class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      loading: false,
-      apod: {},
+      apod: [],
     }
   }
 
@@ -15,13 +15,13 @@ export default class App extends React.Component {
     fetch('https://api.nasa.gov/planetary/apod?api_key=xmVLeY7ByIptpRo2BL7kIKx7GVmsxK23obFsTRiS')
     .then(res => res.json())
     .then(data => {
+
       console.log(data)
       console.log(data.date)
 
-        // this.setState({
-        //   loading: false,
-        //   apod: data.results
-        // })
+        this.setState({
+          apod: data
+        })
     })
     .catch(err => console.log(err))
 
@@ -31,6 +31,8 @@ export default class App extends React.Component {
     return (
       <main className='App'>
         <p>Hi</p>
+        <h1>{this.state.apod.date}</h1>
+        {/* <Item apod={this.state.apod}/> */}
       </main>
     );
   }
